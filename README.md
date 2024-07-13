fabef07f99a81425a98c05cc3c75767ad3fda281
SleepWalker <SleepWalker@qq.com>
2024-07-14 00:17:36 +0800

    feat: - shorten中添加Response - add Dockerfile - update README.md

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Url-Shorten
+
+## 项目架构
+
+```mermaid
+graph TB
+    A[User] --> B[Golang Fiber] -->|存储原始和缩短的url| C[Redis]
+```
+
+```mermaid
+graph LR
+    API --> Database & Helpers & Routes & Dockerfile
+```
+
+
+
+## 测试结果
+
+![](assets/2024-07-13-23-49-09-image.png)
--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -42,7 +42,6 @@ func ShortenUrl(c *fiber.Ctx) error {
 	if err == redis.Nil { // 该IP不存在
 		_ = r.Set(database.Ctx, c.IP(), os.Getenv("API_QUOTA"), time.Minute*30).Err()
 	} else { // 该IP已存在
-		val, _ = r.Get(database.Ctx, c.IP()).Result()
 		count, _ := strconv.Atoi(val)
 		if count <= 0 { // 超过api配额，默认是10
 			limit, _ := r.TTL(database.Ctx, c.IP()).Result()
@@ -66,7 +65,7 @@ func ShortenUrl(c *fiber.Ctx) error {
 	body.URL = helpers.ForceHTTPS(body.URL)
 
 	var id string
-	if body.CustomShortURL != "" {
+	if body.CustomShortURL == "" {
 		id = uuid.New().String()[:6]
 	} else {
 		id = body.CustomShortURL
@@ -96,5 +95,21 @@ func ShortenUrl(c *fiber.Ctx) error {
 	// 速率配额减一，前面都通过才执行
 	_ = r.Decr(database.Ctx, c.IP()).Err()
 
-	return nil
+	// 响应
+	resp := response{
+		URL:             body.URL,
+		CustomShortURL:  "",
+		Expiry:          body.Expiry,
+		XRateRemaining:  10,
+		XRateLimitReset: 30,
+	}
+	val, _ = r.Get(database.Ctx, c.IP()).Result()
+	resp.XRateRemaining, _ = strconv.Atoi(val)
+
+	ttl, _ := r.TTL(database.Ctx, c.IP()).Result()
+	resp.XRateLimitReset = ttl / time.Nanosecond / time.Minute
+
+	resp.CustomShortURL = os.Getenv("DOMAIN") + "/" + id
+
+	return c.Status(fiber.StatusOK).JSON(resp)
 }
