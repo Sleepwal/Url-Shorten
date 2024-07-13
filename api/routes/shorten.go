428e6069308e5f2e5f741a1ef50489395d6110c4
SleepWalker <SleepWalker@qq.com>
2024-07-13 22:16:39 +0800

    feat: 短链接为key，url为值，存储到redis中

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -4,6 +4,7 @@ import (
 	"github.com/asaskevich/govalidator"
 	"github.com/go-redis/redis/v8"
 	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
 	"os"
 	"sleepwalker/url-shorten/database"
 	"sleepwalker/url-shorten/helpers"
@@ -33,8 +34,8 @@ func ShortenUrl(c *fiber.Ctx) error {
 
 	// 速率限制
 	r := database.CreateClient(1)
-	defer func(r2 *redis.Client) {
-		_ = r2.Close()
+	defer func(r *redis.Client) {
+		_ = r.Close()
 	}(r)
 
 	val, err := r.Get(database.Ctx, c.IP()).Result()
@@ -63,6 +64,34 @@ func ShortenUrl(c *fiber.Ctx) error {
 	}
 	// 强制https，SSL
 	body.URL = helpers.ForceHTTPS(body.URL)
+
+	var id string
+	if body.CustomShortURL != "" {
+		id = uuid.New().String()[:6]
+	} else {
+		id = body.CustomShortURL
+	}
+
+	r2 := database.CreateClient(0)
+	defer func(r2 *redis.Client) {
+		_ = r2.Close()
+	}(r2)
+	// 短链接为key，查询
+	val, _ = r2.Get(database.Ctx, id).Result()
+	if val != "" {
+		return c.Status(fiber.StatusForbidden).JSON(fiber.Map{
+			"error": "custom short URL is already in use",
+		})
+	}
+
+	if body.Expiry == 0 {
+		body.Expiry = 24
+	}
+
+	err = r2.Set(database.Ctx, id, body.URL, body.Expiry*time.Hour).Err()
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "database error"})
+	}
 
 	// 速率配额减一，前面都通过才执行
 	_ = r.Decr(database.Ctx, c.IP()).Err()
